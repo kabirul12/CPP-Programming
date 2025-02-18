811711da445a5e3395d81e74e173c9eab48a54f4
Kabirul Hasan <kabirul12ab@gmail.com>
2025-02-18 21:01:58 +0600

    Multiple linear Search

---
+++ Linear_M_search.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int array[]={10,15,10,20,10,25,10,15,24,7};
+    int size=sizeof(array)/sizeof(array[0]);
+    int num;
+    bool found=false;
+
+    cout <<"Enter the value to search: ";
+    cin >>num;
+
+    cout <<"The value is found at positions: ";
+    for(int i=0;i<size;i++) {
+        if(num==array[i]) {
+            cout << (i+1) <<" ";
+            found=true;
+        }
+    }
+    if (found) {
+        cout << endl;
+    } else {
+        cout << "Value not found in the array." << endl;
+    }
+
+    cout <<endl;
+    return 0;
+}
